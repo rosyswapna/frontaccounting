c9c020744ed9e99c3871625591116a0a503f11fd
Rosy Swapna <rosy.swapna@acube.co>
2014-05-30 12:18:17 +0530

    close shipment

--- modules/salescontainer/container_details_entry.php
+++ modules/salescontainer/container_details_entry.php
@@ -16,7 +16,6 @@ include($path_to_root . "/includes/db_pager.inc");
 include_once($path_to_root . "/includes/session.inc");
 
 $js = "";
-
 if ($use_date_picker)
 	$js .= get_js_date_picker();
 
@@ -32,7 +31,7 @@ if (isset($_GET['shipping_id']))
 }
 $selected_id = get_post('shipping_id','');
 
-$close_shipment_id = -1;
+
 //---------------------------------------------------------------------------
 
 
@@ -44,7 +43,7 @@ if (isset($_GET['ModifyShipment'])) {
 
 	$_SESSION['page_title'] = _($help_context = "Close Shipment");
 
-	$close_shipment_id = $_GET['CloseShipment'];
+	$selected_id = $_GET['CloseShipment'];
 
 
 }else{
@@ -55,7 +54,7 @@ if (isset($_GET['ModifyShipment'])) {
 
 //---------------------------------------------------------------------------
 
-function shipping_details_settings($selected_id){
+function shipping_details($selected_id){
 	global $path_to_root;
 	if($selected_id){
 		$myrow = get_shipping_detail($selected_id);
@@ -128,59 +127,50 @@ function shipping_details_settings($selected_id){
 
 function open_shipping_details_settings($selected_id){
 	global $path_to_root;
+
 	if($selected_id){
+
 		$myrow = get_shipping_detail($selected_id);
 		
 		$_POST['shipping_id'] = $myrow["shipping_id"];
 		$_POST['customer_id'] = $myrow["debtor_no"];
 		$_POST['vehicle_details']  = $myrow["vehicle_details"];
-		$_POST['shipment_status']  = $myrow["shipment_status"];
 		$_POST['first_weight']  = $myrow["first_weight"];
 		$_POST['first_weight_date']  = $myrow["first_weight_date"];
-		$_POST['second_weight']  = $myrow["second_weight"];
-		if($myrow["second_weight_date"]!='0000-00-00 00:00:00')
-			$_POST['second_weight_date']  = $myrow["second_weight_date"];
+		$_POST['shipment_status']  = $myrow["shipment_status"];
 
 	}else{
 
 		$_POST['shipping_id'] = $_POST['customer_id'] = -1;
 		$_POST['vehicle_details']  = '';
-		$_POST['shipment_status']  = 1;
 		$_POST['first_weight']  = '';
-		$_POST['first_weight_date']  = '';
-		$_POST['second_weight']  = '';
-		$_POST['second_weight_date']  = '';
-		
+		$_POST['first_weight_date']  = '';	
+		$_POST['shipment_status']  = SHIPMENT_STATUSOPEN;	
 	}
 
 
-	
+	start_table(TABLESTYLE, "width=60%", 10);
 
-	start_table(TABLESTYLE, "width=80%", 10);
 		start_row();
+
 			customer_list_cells(_("Customer:"), 'customer_id', $_POST['customer_id'], false, false, false, true);
 
 			vehicle_cells(_("Vehicle Number").':', 'vehicle_details', _(''), $_POST['vehicle_details'], '');
 
-			shipment_status_cells(_("Shipment Status").':', 'shipment_status', _(''), $_POST['shipment_status']);
-
 		end_row();
 
 		start_row();
-			echo "<td colspan='6'>";
+
+			echo "<td colspan='4'>";
 				start_outer_table(TABLESTYLE2);
 					table_section(1);
 					table_section_title(_("First Weight Details"));
 						first_weight_row(_("First Weight").':', 'first_weight', _(''), $_POST['first_weight'], '');
 						date_row(_("First Weight Date").':', 'first_weight_date', _(''), $_POST['first_weight_date'], '');
-
-					table_section(2);
-					table_section_title(_("Second Weight Details"));
-						first_weight_row(_("Second Weight").':', 'second_weight', _(''), $_POST['second_weight'], '');
-						date_row(_("Second Weight Date").':', 'second_weight_date', _(''), $_POST['second_weight_date'], '');
+					
 				end_outer_table(1);
-			
 			echo "</td>";
+
 		end_row();
 
 	end_table(2);
@@ -199,56 +189,52 @@ function open_shipping_details_settings($selected_id){
 
 function close_shipping_details_settings($selected_id){
 	global $path_to_root;
+
 	if($selected_id){
 		$myrow = get_shipping_detail($selected_id);
 		
 		$_POST['shipping_id'] = $myrow["shipping_id"];
-		$_POST['customer_id'] = $myrow["debtor_no"];
-		$_POST['vehicle_details']  = $myrow["vehicle_details"];
-		$_POST['shipment_status']  = $myrow["shipment_status"];
-		$_POST['first_weight']  = $myrow["first_weight"];
-		$_POST['first_weight_date']  = $myrow["first_weight_date"];
-		$_POST['second_weight']  = $myrow["second_weight"];
-		if($myrow["second_weight_date"]!='0000-00-00 00:00:00')
-			$_POST['second_weight_date']  = $myrow["second_weight_date"];
+		
+
+		if($myrow["shipment_status"] == SHIPMENT_STATUSCLOSE){
+			$_POST['shipment_status']  = $myrow["shipment_status"];
+		}else{
+			$_POST['shipment_status'] = SHIPMENT_STATUSCLOSE;
+		}	
 
 	}else{
 
-		$_POST['shipping_id'] = $_POST['customer_id'] = -1;
-		$_POST['vehicle_details']  = '';
-		$_POST['shipment_status']  = 1;
-		$_POST['first_weight']  = '';
-		$_POST['first_weight_date']  = '';
-		$_POST['second_weight']  = '';
-		$_POST['second_weight_date']  = '';
-		
+	
 	}
-
-
 	
 
-	start_table(TABLESTYLE, "width=80%", 10);
+	start_table(TABLESTYLE, "width=60%", 10);
 		start_row();
-			customer_list_cells(_("Customer:"), 'customer_id', $_POST['customer_id'], false, false, false, true);
 
-			vehicle_cells(_("Vehicle Number").':', 'vehicle_details', _(''), $_POST['vehicle_details'], '');
-
-			shipment_status_cells(_("Shipment Status").':', 'shipment_status', _(''), $_POST['shipment_status']);
+			label_cells(_("Customer:"), $myrow["customer"]);
+			
+			label_cells(_("Vehicle Number:"), $myrow["vehicle_details"]);
 
 		end_row();
 
 		start_row();
-			echo "<td colspan='6'>";
+			echo "<td colspan='4'>";
 				start_outer_table(TABLESTYLE2);
 					table_section(1);
 					table_section_title(_("First Weight Details"));
-						first_weight_row(_("First Weight").':', 'first_weight', _(''), $_POST['first_weight'], '');
-						date_row(_("First Weight Date").':', 'first_weight_date', _(''), $_POST['first_weight_date'], '');
+
+						label_row(_("First Weight:"), $myrow["first_weight"]);
+
+						label_row(_("First Weight Date:"), $myrow["first_weight_date"]);
+					
 
 					table_section(2);
 					table_section_title(_("Second Weight Details"));
+
 						first_weight_row(_("Second Weight").':', 'second_weight', _(''), $_POST['second_weight'], '');
+
 						date_row(_("Second Weight Date").':', 'second_weight_date', _(''), $_POST['second_weight_date'], '');
+
 				end_outer_table(1);
 			
 			echo "</td>";
@@ -257,13 +243,8 @@ function close_shipping_details_settings($selected_id){
 	end_table(2);
 
 	div_start('controls');
-		if (!$selected_id)
-		{
-			//submit_center('submit', _("Add New Shipping Details"), true, '', 'default');
-			submit_center('submit', _("Add New"), true, 'Add New Shipping Details');
-		}else{
-			submit_center('submit','Update',true);
-		}
+		if ($selected_id)
+			submit_center('close','Update',true);
 	div_end();
 	
 }
@@ -290,7 +271,7 @@ function can_process()
 //---------------------------------------------------------------------------
 
 
-//form submit function
+//form submit function for open shipment
 function handle_submit(&$selected_id)
 {
 
@@ -302,7 +283,7 @@ function handle_submit(&$selected_id)
 	if ($selected_id) 
 	{
 		//it is an existing shipping details
-		update_shipping_details($_POST['shipping_id'],$_POST['customer_id'],$_POST['vehicle_details']);
+		update_shipping_details($selected_id,$_POST['customer_id'],$_POST['vehicle_details'],$_POST['shipment_status'],$_POST['first_weight'],$_POST['first_weight_date']);
 		display_notification(_("Shipping details has been updated."));
 
 	} 
@@ -310,12 +291,13 @@ function handle_submit(&$selected_id)
 	{ 	//it is a new entry
 		begin_transaction();
 
-		add_shipping_details($_POST['customer_id'],$_POST['vehicle_details'],$_POST['shipment_status'],$_POST['first_weight'],$_POST['first_weight_date'],$_POST['second_weight'],$_POST['second_weight'],$_POST['second_weight_date']);
+		add_shipping_details($_POST['customer_id'],$_POST['vehicle_details'],$_POST['shipment_status'],$_POST['first_weight'],$_POST['first_weight_date']);
 		$selected_id = $_POST['shipping_id'] = db_insert_id();
 
 		commit_transaction();
 
 		display_notification(_("A new shipping details has been added."));
+
 		$Ajax->activate('_page_body');
 	}
 }
@@ -328,6 +310,20 @@ if (isset($_POST['submit']))
 }
 //-------------------------------------------------------------------------------------------- 
 
+//form submit action
+if (isset($_POST['close'])) 
+{
+	global $path_to_root, $Ajax, $auto_create_branch;
+
+	if ($selected_id) 
+	{
+		//it is an existing shipping details
+		close_shipment($selected_id,$_POST['shipment_status'],$_POST['second_weight'],$_POST['second_weight_date']);
+		display_notification(_("Shipping details has been closed."));
+	}
+}
+//-------------------------------------------------------------------------------------------- 
+
 
 add_access_extensions();
 set_ext_domain('modules/salescontainer');
@@ -337,8 +333,10 @@ page($_SESSION['page_title'], false, false, "", $js);
 
 start_form();
 
+
 	if (isset($_GET['CloseShipment'])) {
-		close_shipping_details_settings($close_shipment_id);
+		close_shipping_details_settings($selected_id);
+		hidden('shipping_id');
 	}else{
 
 		if (db_has_shipping_details()) 
@@ -365,6 +363,8 @@ start_form();
 
 		open_shipping_details_settings($selected_id);
 	}
+
+	hidden('shipment_status');
 
 end_form();
 
